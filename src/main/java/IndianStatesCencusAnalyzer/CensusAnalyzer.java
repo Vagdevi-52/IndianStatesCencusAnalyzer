8b5dcff83a429dade63b7d481a9a399c1c3c7bc2
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-26 02:59:33 -0700

    Initial Commit: Welcome To Indian States Cencus Analyzer

---
+++ src/main/java/IndianStatesCencusAnalyzer/CensusAnalyzer.java
@@ -0,0 +1,11 @@
+package IndianStatesCencusAnalyzer;
+
+public class CensusAnalyzer
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To Indian States Census Analyzer");
+	}
+
+}
